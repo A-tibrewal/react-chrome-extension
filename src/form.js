9f04f377ca9711159ae5c4ad36f92ba6b66eb245
a-tibrewal <aditya7bgp@gmail.com>
2018-09-23 21:06:46 +0530

    some more css changes

--- src/form.js
+++ src/form.js
@@ -5,31 +5,31 @@ class Form extends Component {
     return (
       <div className="profile-form">
         <div className="InputAddOn">
-            <label for="phone_number" className="InputAddOn-item">Contact</label>
+            <label className="InputAddOn-item" for="phone_number" >Contact</label>
             <input type="text" required="true" class="form-control" id="phone_number" name="phone_number" placeholder="Phone Number" autocomplete="off" />
         </div>
         <div className="InputAddOn">
-            <label for="email">Email</label>
+            <label className="InputAddOn-item" for="email">Email</label>
             <input type="text" className="form-control" id="email" name="email" placeholder="Email" required="true" />
         </div>
         <div className="InputAddOn">
-            <label for="resume" >Resume</label>
+            <label className="InputAddOn-item" for="resume" >Resume</label>
             <input type="file" className="form-control" id="resume" name="resume" />
         </div>
         <div className="InputAddOn">
-            <label for="name">Name</label>
+            <label className="InputAddOn-item" for="name">Name</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Name" autocomplete="off" required="true" />
         </div>
         <div className="InputAddOn">
-        <label for="university">University</label>
+        <label className="InputAddOn-item" for="university">University</label>
         <input className="form-control" id="university" name="university" required="true" type="text" />
       </div>
       <div className="InputAddOn">
-        <label for="orgyear">Grad Yr</label>
+        <label className="InputAddOn-item" for="orgyear">Grad Yr</label>
         <input type="number" className="form-control" id="orgyear" name="orgyear" placeholder="Graduation Year" autocomplete="off" required="true" type="number" />
       </div>
       <div className="InputAddOn">
-        <label for="degreer">Degree</label>
+        <label className="InputAddOn-item" for="degreer">Degree</label>
         <select class="form-control" type="text" name='degree' id="degree" required="true">
                 <option value="">Choose Degree</option>
                 <option value="BE/B.Tech/BS">BE/B.Tech/BS</option>
@@ -44,7 +44,7 @@ class Form extends Component {
               </select>
       </div>
       <div className="InputAddOn">
-        <label for="field">Field</label>
+        <label className="InputAddOn-item" for="field">Field</label>
         <select className="form-control" type="text" name='field' id="field" required="true">
                 <option value="">Choose Stream</option>
                 <option value="Computer Science">Computer Science</option>
@@ -58,7 +58,7 @@ class Form extends Component {
       </div>
 
       <div className="InputAddOn">
-        <label for="position">Position</label>
+        <label className="InputAddOn-item" for="position">Position</label>
         <select className="form-control" type="text" name='position' id="position"  required="true">
                 <option value="">Choose Job Position</option>
                 <option value="Engineering Leadership">Engineering Leadership</option>
@@ -88,26 +88,26 @@ class Form extends Component {
       </div>
 
       <div className="InputAddOn">
-        <label for="orgname">Company</label>
+        <label className="InputAddOn-item" for="orgname">Company</label>
         <input className="form-control" id="orgname" name="orgname" type="text" />
       </div>
       <div className="InputAddOn">
-        <label for="city">Location</label>
+        <label className="InputAddOn-item" for="city">Location</label>
         <input type="text" className="form-control" id="city" name="city" placeholder="Current Location" autocomplete="off" />
       </div>
 
-      <div >
-        <label >Current CTC (Lac/y)</label>
+
+      <div className="InputAddOn">
+        <label className="InputAddOn-item" >Fixed</label>
+        <input className="form-control" type="number" id="base_ctc" name='base_ctc' placeholder="Fixed income" min='0' max='900'  step="any" />
       </div>
       <div className="InputAddOn">
-        <label >Fixed</label>
-        <input className="form-control" type="number" id="base_ctc" name='base_ctc' placeholder="Fixed income" min='0' max='900'  step="any" />
-        <label className="control-label" >Variable</label>
+        <label className="InputAddOn-item" >Variable</label>
         <input className="form-control" type="number" id="variable_ctc" name='variable_ctc' placeholder="Variable Income" min='0' max='900' step="any" />
       </div>
 
       <div className="InputAddOn">
-        <label  for="notice_period">Notice period</label>
+        <label className="InputAddOn-item"  for="notice_period">Notice</label>
         <select name="notice-period" placeholder="Notice
               period" id="notice_period" className="form-control">
 							<option value="">Select notice period</option>
